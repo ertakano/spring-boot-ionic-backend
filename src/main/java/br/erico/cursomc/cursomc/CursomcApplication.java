0721273fa8632fbc4d4529c1e31ab1240cbb3fac
Erico Takano <erico.rtakano@gmail.com>
2020-03-20 12:06:01 -0300

    projeto iniciado

---
+++ src/main/java/br/erico/cursomc/cursomc/CursomcApplication.java
@@ -0,0 +1,13 @@
+package br.erico.cursomc.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+}
