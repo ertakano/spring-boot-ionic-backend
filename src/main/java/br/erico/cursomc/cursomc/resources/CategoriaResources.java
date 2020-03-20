29c9fc9097ccf3504beb50cb6cdb7d33cc67ec6a
Erico Takano <erico.rtakano@gmail.com>
2020-03-20 15:45:17 -0300

    Testandoo a primeira classe de dominio - Categoria

---
+++ src/main/java/br/erico/cursomc/cursomc/resources/CategoriaResources.java
@@ -0,0 +1,28 @@
+package br.erico.cursomc.cursomc.resources;
+
+import br.erico.cursomc.cursomc.domain.Categoria;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResources {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Categoria> listar() {
+
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Escritório");
+
+        List<Categoria> lista = new ArrayList<Categoria>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
+     }
+
+}
