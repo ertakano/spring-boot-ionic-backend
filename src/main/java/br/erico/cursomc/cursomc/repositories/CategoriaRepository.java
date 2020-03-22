b734281de9d80190ade3163fe618729971523cd8
Erico Takano <erico.rtakano@gmail.com>
2020-03-22 12:08:54 -0300

    Criando repository e service para categoria

---
+++ src/main/java/br/erico/cursomc/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,8 @@
+package br.erico.cursomc.cursomc.repositories;
+
+import br.erico.cursomc.cursomc.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {}
