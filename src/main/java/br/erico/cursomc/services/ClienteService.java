79bff287a463a30e451c072dd92e86b852d626ea
Erico Takano <erico.rtakano@gmail.com>
2020-03-26 11:32:48 -0300

    Endpoint /clientes/{id} disponível

---
+++ src/main/java/br/erico/cursomc/services/ClienteService.java
@@ -0,0 +1,25 @@
+package br.erico.cursomc.services;
+
+import br.erico.cursomc.domain.Categoria;
+import br.erico.cursomc.domain.Cliente;
+import br.erico.cursomc.exception.ObjectNotFoundException;
+import br.erico.cursomc.repositories.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository repo;
+
+    public Cliente buscar(Integer id){
+        Optional<Cliente> obj = repo.findById(id);
+        return obj.orElseThrow(
+                () -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName())
+        );
+    }
+
+}
