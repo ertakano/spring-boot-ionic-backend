0ceca5f7deba07662599018016a90682e4ee32eb
Erico Takano <erico.rtakano@gmail.com>
2020-03-26 16:50:04 -0300

    Pedido, EstadoPagamento, Pagamento

---
+++ src/main/java/br/erico/cursomc/repositories/PedidoRepository.java
@@ -0,0 +1,8 @@
+package br.erico.cursomc.repositories;
+
+import br.erico.cursomc.domain.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {}
