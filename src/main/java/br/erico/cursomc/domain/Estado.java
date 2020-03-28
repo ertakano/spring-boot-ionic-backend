b6a3235491bb01ccf93af171c84eefbdbd6495db
Erico Takano <erico.rtakano@gmail.com>
2020-03-28 10:44:01 -0300

    Atualizacao utilizando somente JsonIgnore

--- src/main/java/br/erico/cursomc/domain/Estado.java
+++ src/main/java/br/erico/cursomc/domain/Estado.java
@@ -1,8 +1,6 @@
 package br.erico.cursomc.domain;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import org.springframework.boot.autoconfigure.web.ResourceProperties;
-
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -18,7 +16,7 @@ public class Estado implements Serializable {
     private Integer id;
     private String nome;
 
-    @JsonBackReference
+    @JsonIgnore
     @OneToMany(mappedBy = "estado")
     private List<Cidade> cidades = new ArrayList<>();
 
