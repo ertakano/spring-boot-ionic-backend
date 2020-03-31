7ec0dc7dee44c4fb8c6b8d437143a5838c9aadc6
Erico Takano <erico.rtakano@gmail.com>
2020-03-31 11:39:57 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/br/erico/cursomc/services/CategoriaService.java
+++ src/main/java/br/erico/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import br.erico.cursomc.services.exception.ObjectNotFoundException;
 import br.erico.cursomc.services.exception.DataIntegrityException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import br.erico.cursomc.repositories.CategoriaRepository;
 
@@ -17,33 +20,39 @@ public class CategoriaService {
     @Autowired
     private CategoriaRepository repo;
 
-    public Categoria find(Integer id){
+    public Categoria find(Integer id) {
         Optional<Categoria> obj = repo.findById(id);
         return obj.orElseThrow(
                 () -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName())
         );
     }
 
-    public Categoria insert(Categoria obj){
+    public Categoria insert(Categoria obj) {
         obj.setId(null);
         return repo.save(obj);
     }
 
-    public Categoria update(Categoria obj){
+    public Categoria update(Categoria obj) {
         find(obj.getId());
         return repo.save(obj);
     }
 
-    public void delete(Integer id){
+    public void delete(Integer id) {
         find(id);
-        try{
+        try {
             repo.deleteById(id);
-        }catch (DataIntegrityViolationException e){
+        } catch (DataIntegrityViolationException e) {
             throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos.");
         }
     }
 
-    public List<Categoria> findAll(){
+    public List<Categoria> findAll() {
         return repo.findAll();
     }
+
+    public Page<Categoria> findPage(Integer page, Integer linesPerPage, String direction, String orderBy) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        return repo.findAll(pageRequest);
+    }
+
 }
